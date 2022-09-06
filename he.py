5edcfd6febb2f93d72e9d5f4c68b0bc420a453d4
bouboo1 <475174920@qq.com>
2022-09-06 23:24:15 +0800

    ok .

---
+++ he.py
@@ -0,0 +1,9 @@
+'''
+Author: bouboo1 475174920@qq.com
+Date: 2022-09-06 23:23:07
+LastEditors: bouboo1 475174920@qq.com
+LastEditTime: 2022-09-06 23:23:12
+FilePath: \bouboo\he.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE'
+'''
+print ('helloworld!')
